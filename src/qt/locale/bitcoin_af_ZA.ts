f086ce19fa3ef43a1d5aea5e329bcf12c92177ec
rwdim <me@rwd.im>
2018-11-16 15:32:54 +0000

    Updated locales to use BitCurrency where appropriate

--- src/qt/locale/bitcoin_af_ZA.ts
+++ src/qt/locale/bitcoin_af_ZA.ts
@@ -155,7 +155,7 @@
     </message>
     <message>
         <source>Bitcoin</source>
-        <translation>Blackcoin</translation>
+        <translation>BitCurrency</translation>
     </message>
     <message>
         <source>Wallet</source>
